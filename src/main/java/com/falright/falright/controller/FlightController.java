c4b73f097e32a151d562df0d59890d83046361d6
Krzysztof Bicz <kristofarer@gmail.com>
2024-05-19 19:49:46 +0200

    Update FlightController.java

--- src/main/java/com/falright/falright/controller/FlightController.java
+++ src/main/java/com/falright/falright/controller/FlightController.java
@@ -13,6 +13,8 @@ import org.springframework.ui.Model;
 
 import org.springframework.web.bind.annotation.*;
 
+
+import java.util.ArrayList;
 import java.util.Optional;
 
 import org.springframework.web.bind.annotation.GetMapping;
